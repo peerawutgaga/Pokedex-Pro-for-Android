3510271ac93926f194d0fd406cf038d47ce9a21d
Peerawut <Peerawut@PEERAWUT-PC>
2017-08-03 14:20:05 +0700

    Finish mainUI

--- Pokedex Pro for Android/MainActivity.cs
+++ Pokedex Pro for Android/MainActivity.cs
@@ -4,15 +4,13 @@ using Android.OS;
 
 namespace Pokedex_Pro_for_Android
 {
-    [Activity(Label = "Pokedex_Pro_for_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Pokedex Pro for Android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
         }
     }
 }
--- Pokedex Pro for Android/Resources/Resource.Designer.cs
+++ Pokedex Pro for Android/Resources/Resource.Designer.cs
@@ -45,7 +45,34 @@ namespace Pokedex_Pro_for_Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ability_icon = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int egg_group_icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int item_icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int location_icon = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int move_icon = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int nature_icon = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int pokedex_icon = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int setting_icon = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int type_icon = 2130837513;
 			
 			static Drawable()
 			{
@@ -53,6 +80,49 @@ namespace Pokedex_Pro_for_Android
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050006
+			public const int abilityButton = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int eggGroupButton = 2131034116;
+			
+			// aapt resource value: 0x7f050000
+			public const int gridLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050007
+			public const int itemButton = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int locationButton = 2131034120;
+			
+			// aapt resource value: 0x7f050003
+			public const int moveButton = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int natureButton = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int pokedexButton = 2131034113;
+			
+			// aapt resource value: 0x7f050009
+			public const int settingButton = 2131034121;
+			
+			// aapt resource value: 0x7f050002
+			public const int typeButton = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
